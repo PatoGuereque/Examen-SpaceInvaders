f881defeb87379dbe661475658f2aa7f8e3455e8
Rafaelmor2000 <Rafaelmor2000@gmail.com>
2020-04-04 01:25:22 -0600

    documented utilities

--- src/spaceinvaders/util/SpriteSheet.java
+++ src/spaceinvaders/util/SpriteSheet.java
@@ -12,15 +12,24 @@ import java.awt.image.BufferedImage;
  * @author insan
  */
 public class SpriteSheet {
-    private BufferedImage sheet;
+    private BufferedImage sheet;            //hold spritesheet
+    
     /**
-     * 
-     * @param sheet 
+     * load spritesheet
+     * @param sheet image with spritesheet
      */
     public SpriteSheet(BufferedImage sheet){
         this.sheet = sheet;
     }
     
+    /**
+     * cut spritesheet into individual frames
+     * @param x x position
+     * @param y y position
+     * @param width width value
+     * @param height height value
+     * @return individual frame
+     */
     public BufferedImage crop(int x, int y, int width, int height){
         return sheet.getSubimage(x, y, width, height);
     }
